5ee4c2fd7222f544835ef3d1e372f532057cef52
Aditya Candra Yuki Bintoro <112878110+AdityaCandraYuki@users.noreply.github.com>
2023-07-12 17:25:07 +0700

    Mempersiapkan komponen button
    Membuat komponen button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import propTypes from 'prop-types'
 export default function Button(props) {
   return (
-    <div>
-
-    </div>
+    <div></div>
   )
+}
+
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]),
+  onclick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.string,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  hasShadow: propTypes.bool,
 }
